f8378463f24aff0236b28e23b3ac74795f858729
Zhijie Xia <76631942+zhijie-os@users.noreply.github.com>
2023-07-25 10:58:44 -0400

    supabase upload and download

--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,58 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+import { createClient } from "@supabase/supabase-js";
+import { useRef } from "react";
+
+// create supabase client with anon key
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  {
+    persistSession: false,
+  }
+);
 
 export default function Home() {
+  const imageRef = useRef(null);
+
+  const handleUpload = async (e) => {
+    let file;
+
+    if (e.target.files) {
+      file = e.target.files[0];
+    }
+    // upload the file to the 'FooBucket' bucket in 'public' folder
+    const { data, error } = await supabase.storage
+      .from("FooBucket")
+      .upload("public/" + file.name, file);
+    console.log(file);
+    console.log(data, error);
+  };
+
+  const handleDownload = async () => {
+    // download the file from the 'FooBucket' bucket in 'public' folder
+    const { data, error } = await supabase.storage
+      .from("FooBucket")
+      .download("public/" + "foo.png");
+    console.log(data, error);
+    // set the image source using the downloaded data
+    imageRef.current.src = URL.createObjectURL(data);
+  };
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <input
+        type="file"
+        accept="image/*"
+        className="block w-auto text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300"
+        id="file_input"
+        onChange={(e) => {
+          handleUpload(e);
+        }}
+      />
+      <button onClick={handleDownload}>click me</button>
+      <img ref={imageRef}></img>
     </div>
-  )
+  );
 }
